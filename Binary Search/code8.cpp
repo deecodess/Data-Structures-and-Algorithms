4ac8a2585192700dc98f3a55714c5afedbf02900
Deepanshi Sharma <90607584+deecodess@users.noreply.github.com>
2022-07-18 18:59:41 +0530

    bin-sea

---
+++ Binary Search/code8.cpp
@@ -0,0 +1,45 @@
+/* Book Allocation Problem */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPossible(vector <int> arr, int n, int m, int mid){
+    int studentCount=1;
+    int pageSum=0;
+    for(int i=0;i<n;i++){
+        if(pageSum+arr[i]<=mid){
+            pageSum +=arr[i];
+        }
+        else{
+            studentCount++;
+            if(studentCount>m||arr[i]>mid){
+                return false;
+            }
+            pageSum=arr[i];
+        }
+    }
+   return true; 
+}
+
+int allocateBooks(vector<int> arr, int n, int m) {
+    // Write your code here.
+    int s=0;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    int e=sum;
+    int mid= s+ (e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if(isPossible(arr,n,m,mid)){
+            ans=mid;
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
