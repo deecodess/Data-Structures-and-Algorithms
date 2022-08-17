2e6be1666cad11559e06c9b8ded351ef5e5bde69
Deepanshi Sharma <90607584+deecodess@users.noreply.github.com>
2022-08-17 23:38:44 +0530

    new stl code added

---
+++ STL/deque.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(){
+
+    deque <int> d;
+
+    d.push_back(1);
+    d.push_front(2);
+
+    for(int i:d){
+        cout<<i<<" ";
+    }cout<<endl;
+
+    d.pop_front();
+    for(int i:d){
+        cout<<i<<" ";
+    }cout<<endl;
+
+}
